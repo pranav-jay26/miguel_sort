3fc96462b2cbc97f7a8ef9ac70cc1b017264553f
pranav-jay26 <p_jayakumar@u.pacific.edu>
2025-02-07 15:58:45 -0800

    Add python implementation

---
+++ miguel_sort.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def miguel_sort(arr: list[int]) -> list[int]:
+    bools: list[bool] = [False for _ in range(2**32)]
+
+    for x in arr:
+        bools[x] = True
+
+    return [i for i, b in enumerate(bools) if b]
